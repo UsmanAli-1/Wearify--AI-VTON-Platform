05611ff9b029c483e61f24034333e100d64cc10b
Usman Ali <usmanali0044444@gmail.com>
2026-03-06 18:22:16 +0500

    Fix header filename casing

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import BackgroundDots from "@/components/BackgroundDots";
-import Header from "@/components/Header";
+import Header from "@/components/header-temp";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
